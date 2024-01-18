97556a8d666b35a7d8b7ce758186e9d3ec3659fd
Jason <jason202005@gmail.com>
2024-01-19 00:21:24 +0800

    Add dockerfile & scripts

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+# Add Docker's official GPG key:
+sudo apt-get update -y
+sudo apt-get install ca-certificates curl gnupg -y
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/k
+eyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://
+download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update 
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-com
+pose-plugin -y
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+sudo apt install python3 python3-pip -y
+sudo chmod +x docker_build.sh
